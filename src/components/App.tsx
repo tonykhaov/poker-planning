2c0d08825fb12332dc5c121ce9f94c7c791d14d3
Tony Khaov <tony.khaov@gmail.com>
2021-04-23 23:40:36 +0200

    feat: add firebase auth + database + firestore

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react'
+import firebase from 'firebase/app'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useDocumentData } from 'react-firebase-hooks/firestore'
 
 function App() {
+  const [user, loading, error] = useAuthState(firebase.auth())
+  const [value] = useDocumentData(firebase.firestore().doc('users/XdcaOF0moHeAyCGJs8g9'), {
+    snapshotListenOptions: { includeMetadataChanges: true },
+  })
+  const username = value?.[user?.uid ?? '']
+  console.log(user)
   return (
     <div>
       <h1 className="text-2xl text-center uppercase">Poker planning</h1>
@@ -9,6 +18,30 @@ function App() {
         <button className="border border-gray-500">Join a room</button>
 
         <button className="border border-gray-500">Create a room</button>
+      </div>
+
+      <div>
+        <p>login</p>
+        {username ? username : null}
+        <button
+          onClick={() =>
+            firebase
+              .auth()
+              .signInAnonymously()
+              .then((res) => {
+                if (!res.user?.uid) return
+                firebase
+                  .firestore()
+                  .collection('users')
+                  .add({
+                    [res.user.uid]: 'luc',
+                  })
+              })
+          }
+        >
+          Log in
+        </button>
+        <button onClick={() => firebase.auth().signOut()}>Logout</button>
       </div>
     </div>
   )
--- src/context/app-providers.tsx
+++ src/context/app-providers.tsx
@@ -1,9 +1,32 @@
 import * as React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { FirebaseDatabaseProvider } from '@react-firebase/database'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import { FirebaseAuthProvider } from '@react-firebase/auth'
+import { FirestoreProvider } from '@react-firebase/firestore'
 
 const client = new QueryClient()
 
-// TODO find the best type to make rtlRender work
+export const config = {
+  apiKey: 'AIzaSyA_3pH_mSGLlruZhc935W_usggb7I0otwo',
+  authDomain: 'poker-planning-5c616.firebaseapp.com',
+  projectId: 'poker-planning-5c616',
+  storageBucket: 'poker-planning-5c616.appspot.com',
+  messagingSenderId: '251954642401',
+  appId: '1:251954642401:web:882cfd8616101926df26a0',
+  databaseURL: 'https://poker-planning-5c616-default-rtdb.europe-west1.firebasedatabase.app/',
+}
+
 export default function AppProviders({ children }: any) {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  return (
+    <FirestoreProvider firebase={firebase} {...config}>
+      <FirebaseAuthProvider firebase={firebase} {...config}>
+        <FirebaseDatabaseProvider firebase={firebase} {...config}>
+          <QueryClientProvider client={client}>{children}</QueryClientProvider>
+        </FirebaseDatabaseProvider>
+      </FirebaseAuthProvider>
+    </FirestoreProvider>
+  )
 }
