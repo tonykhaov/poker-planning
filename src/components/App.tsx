a6b437365ab72d086d24441ed5fd4a3a57b7175d
Tony Khaov <tony.khaov@gmail.com>
2021-04-24 19:30:11 +0200

    feat: create db document on sign in with user's name

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,43 +1,59 @@
 import * as React from 'react'
 import firebase from 'firebase/app'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { useCollection } from 'react-firebase-hooks/firestore'
+import { useCollection, useDocument } from 'react-firebase-hooks/firestore'
 
 function App() {
   const [user, loading, error] = useAuthState(firebase.auth())
-  const [value, ...coll] = useCollection(firebase.firestore().collection('usernames'))
+  const userID = user?.uid ?? ''
+  const usersRef = firebase.firestore().collection('users')
+  const [collection] = useCollection(usersRef.where('uid', '==', userID))
+  const documentID = collection?.docs[0]?.id
+  const [document] = useDocument(firebase.firestore().doc(`users/${documentID}`))
+
+  const [username, setUsername] = React.useState('')
+  const signOut = () => firebase.auth().signOut()
+  const signIn = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (username === '') return
+    firebase
+      .auth()
+      .signInAnonymously()
+      .then((res) => {
+        if (!res.user?.uid) return
+        usersRef.add({
+          name: username,
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+          uid: res.user?.uid,
+        })
+      })
+      .then(() => setUsername(''))
+  }
 
   return (
     <div>
       <h1 className="text-2xl text-center uppercase">Poker planning</h1>
-
       <div>
-        <button className="border border-gray-500">Join a room</button>
-
-        <button className="border border-gray-500">Create a room</button>
-      </div>
-
-      <div>
-        <p>login</p>
-        <button
-          onClick={() =>
-            firebase
-              .auth()
-              .signInAnonymously()
-              .then((res) => {
-                if (!res.user?.uid) return
-                firebase
-                  .firestore()
-                  .collection('usernames')
-                  .add({
-                    [res.user.uid]: 'luc',
-                  })
-              })
-          }
-        >
-          Log in
-        </button>
-        <button onClick={() => firebase.auth().signOut()}>Logout</button>
+        {user ? <h1 className="text-xl">{document?.data()?.name}</h1> : null}
+        {user ? (
+          <button onClick={signOut} className="px-8 py-2 border border-gray-700 rounded-md">
+            Log out
+          </button>
+        ) : (
+          <form onSubmit={signIn}>
+            <input
+              type="text"
+              className="px-8 py-2 border border-gray-700 rounded-sm"
+              placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.currentTarget.value)}
+              required
+            />
+            <button onClick={signIn} className="px-8 py-2 border border-gray-700 rounded-md">
+              Log in
+            </button>
+          </form>
+        )}
       </div>
     </div>
   )
