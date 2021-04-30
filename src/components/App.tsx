2adb653751126bcad4eda6b88b25393a1f851a2c
Tony Khaov <tony.khaov@gmail.com>
2021-04-30 19:20:22 +0200

    refactor: remove input state to prevent excessive rerenders

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,13 @@ import firebase from 'firebase/app'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollection, useDocument } from 'react-firebase-hooks/firestore'
 
+interface FormElements extends HTMLFormControlsCollection {
+  username: HTMLInputElement
+}
+interface UsernameFormElement extends HTMLFormElement {
+  readonly elements: FormElements
+}
+
 function App() {
   const [user, loading, error] = useAuthState(firebase.auth())
   const userID = user?.uid ?? ''
@@ -11,47 +18,45 @@ function App() {
   const documentID = collection?.docs[0]?.id
   const [document] = useDocument(firebase.firestore().doc(`users/${documentID}`))
 
-  const [username, setUsername] = React.useState('')
   const signOut = () => firebase.auth().signOut()
-  const signIn = (e: React.FormEvent) => {
+  const signIn = async (e: React.FormEvent<UsernameFormElement>) => {
     e.preventDefault()
+    const { username: usernameInput } = e.currentTarget.elements
+    const username = usernameInput.value
+
     if (username === '') return
-    firebase
-      .auth()
-      .signInAnonymously()
-      .then((res) => {
-        if (!res.user?.uid) return
-        usersRef.add({
-          name: username,
-          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-          uid: res.user?.uid,
-        })
-      })
-      .then(() => setUsername(''))
+
+    const res = await firebase.auth().signInAnonymously()
+    if (!res.user?.uid) return
+
+    usersRef.add({
+      name: username,
+      uid: res.user.uid,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    })
   }
 
   return (
     <div>
       <h1 className="text-2xl text-center uppercase">Poker planning</h1>
       <div>
-        {user ? <h1 className="text-xl">{document?.data()?.name}</h1> : null}
         {user ? (
-          <button onClick={signOut} className="px-8 py-2 border border-gray-700 rounded-md">
-            Log out
-          </button>
+          <>
+            <h1 className="text-xl">{document?.data()?.name}</h1>
+            <button onClick={signOut} className="px-8 py-2 border border-gray-700 rounded-md">
+              Log out
+            </button>
+          </>
         ) : (
           <form onSubmit={signIn}>
             <input
               type="text"
+              name="username"
               className="px-8 py-2 border border-gray-700 rounded-sm"
               placeholder="Enter your username"
-              value={username}
-              onChange={(e) => setUsername(e.currentTarget.value)}
               required
             />
-            <button onClick={signIn} className="px-8 py-2 border border-gray-700 rounded-md">
-              Log in
-            </button>
+            <button className="px-8 py-2 border border-gray-700 rounded-md">Log in</button>
           </form>
         )}
       </div>
