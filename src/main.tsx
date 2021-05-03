e4cc91d33c7e42cc9611add302d86c89bfa102c7
Tony Khaov <tony.khaov@gmail.com>
2021-05-03 23:17:58 +0200

    chore: remove msw worker in browser

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom'
 import 'tailwindcss/tailwind.css'
 import AppProviders from './context/app-providers'
 import App from './components/App'
-import { worker } from './mocks/browser'
-
-if (process.env.NODE_ENV === 'development') {
-  worker.start()
-}
 
 ReactDOM.render(
   <React.StrictMode>
